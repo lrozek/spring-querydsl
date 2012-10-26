d2d1b314f6d88c28323fe9a701832250ddf67f30
Lukasz Rozek <lukasz.rozek@consileon.pl>
2012-10-26 17:47:29 +0200

    externalized properties

--- src/main/java/pl/lrozek/springQuerydsl/configuration/BootstrapConfiguration.java
+++ src/main/java/pl/lrozek/springQuerydsl/configuration/BootstrapConfiguration.java
@@ -3,19 +3,37 @@ package pl.lrozek.springQuerydsl.configuration;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
+@PropertySource("classpath:/application.props")
 @Configuration
 public class BootstrapConfiguration {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @Bean(destroyMethod = "close")
     public DataSource h2() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName( "net.sf.log4jdbc.DriverSpy" );
-        dataSource.setUrl( "jdbc:log4jdbc:h2:~/.querydsl/querydsl;AUTO_SERVER=TRUE" );
-        dataSource.setUsername( "sa" );
+        dataSource.setDriverClassName( driverClassName );
+        dataSource.setUrl( url );
+        dataSource.setUsername( username );
         return dataSource;
     }
+
+    @Value("${jdbc.driver}")
+    private String driverClassName;
+
+    @Value("${jdbc.url}")
+    private String url;
+
+    @Value("${jdbc.username}")
+    private String username;
 
 }
